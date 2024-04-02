9a17ee3ae443955c2062c1357df2b55d11d7f48a
charris6 <155675640+charris6@users.noreply.github.com>
2024-04-02 16:19:30 -0600

    Update pgsql_magic.py

--- DSToolBox/pgsql_magic.py
+++ DSToolBox/pgsql_magic.py
@@ -26,10 +26,10 @@ Example:
 SELECT * FROM my_table
 This would execute the SQL query "SELECT * FROM my_table", store the resulting DataFrame in the results_dict under a key like 'dfmyQuery1', and print "saved to results_dict[dfmyQuery1]".
 """
-  global results_dict  # Refer to the global dictionary
-  query = text(cell)
-  with engine.connect() as conn:
-      result = pd.read_sql_query(query, conn)
-  dict_name = f'df{line}{len(results_dict)+1}'
-  results_dict[dict_name] = result
-  print(f'saved to results_dict[{dict_name}]')
+global results_dict  # Refer to the global dictionary
+query = text(cell)
+with engine.connect() as conn:
+    result = pd.read_sql_query(query, conn)
+dict_name = f'df{line}{len(results_dict)+1}'
+results_dict[dict_name] = result
+print(f'saved to results_dict[{dict_name}]')
