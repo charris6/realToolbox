f55ddec37afac615b5ca06343f908260249ee1f8
charris6 <155675640+charris6@users.noreply.github.com>
2024-04-02 16:26:44 -0600

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,11 @@
-from setuptools import setup, find_packages 
+from setuptools import setup, find_packages
+
+# Read the contents of your requirements.txt file
+with open('requirements.txt') as f:
+    required = f.read().splitlines()
 
 setup(
-    name='realToolbox', 
+    name='realToolbox',
     version='0.1.0',
     author='Colin Harris',
     author_email='colin.james.harris@gmail.com',
@@ -10,7 +14,6 @@ setup(
     license='LICENSE.txt',  # Make sure to specify the correct license file name
     description='This is my data science toolbox for useful code.',
     long_description=open('README.md').read(),
-    install_requires=[
-        'numpy >= 1.11.1'
-    ],
+    long_description_content_type='text/markdown',  # If your README.md is in markdown
+    install_requires=required,
 )
